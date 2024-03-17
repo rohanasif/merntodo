962d52c29be4dcdcd01a2c25aa678683cd585eb8
rohanasif <rohanasif1990@gmail.com>
2024-03-17 14:24:52 +0500

    done: logincontroller and loginroute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,18 @@
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
+import bcryptjs from "bcryptjs";
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
-
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error("All fields are required");
+    }
     const exists = await User.findOne({ email: email });
 
     if (exists) {
@@ -16,10 +20,12 @@ export const registerUser = asyncHandler(async (req, res) => {
       throw new Error("User already exists");
     }
 
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     if (user) {
@@ -33,6 +39,41 @@ export const registerUser = asyncHandler(async (req, res) => {
     } else {
       res.status(400);
       throw new Error("Invalid user data");
+    }
+  } catch (error) {
+    res.status(400);
+    res.json({
+      message: error.message,
+    });
+  }
+});
+
+// @desc    Login user
+// @route   POST /api/users/login
+// @access  Public
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+
+    const match = bcryptjs.compareSync(password, user.password);
+
+    if (match) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
     }
   } catch (error) {
     res.status(400);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
-
+router.post("/login", loginUser);
 export default router;

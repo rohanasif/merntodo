cf1536c156cb73ad1aad316e3f70c8f1e2ab5f0e
rohanasif <rohanasif1990@gmail.com>
2024-03-17 13:21:37 +0500

    Database connection made

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");

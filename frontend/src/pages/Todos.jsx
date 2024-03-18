c1327cf893d1bb8f4669c1e44b7a9b4f733d0097
rohanasif <rohanasif1990@gmail.com>
2024-03-19 02:22:16 +0500

    Disable add button unless title is supplied

--- frontend/src/pages/Todos.jsx
+++ frontend/src/pages/Todos.jsx
@@ -44,7 +44,10 @@ const Todos = () => {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <button className="bg-green-700 py-2 px-4 text-white rounded-xl">
+          <button
+            disabled={!title.length}
+            className="bg-green-700 py-2 px-4 text-white rounded-xl disabled:bg-gray-700"
+          >
             Add
           </button>
         </form>
